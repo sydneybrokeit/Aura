a280baabc6ac601a199c54f89f87f824831bd059
Keaton Burleson <keaton.burleson@me.com>
2016-12-07 23:35:36 -0500

    maybe did something good

--- modules/demo/index.php
+++ modules/demo/index.php
@@ -1,6 +1,6 @@
 <?php
 $root = $_SERVER["DOCUMENT_ROOT"] . "/";
-$settings = json_decode(file_get_contents($root . "config.json"), true);
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 	if (isset($_GET["sku"]) && !empty($_GET["sku"])) {
