66f148a9d5d11bfebe071a6ef9411de694af81ba
Keaton Burleson <keaton.burleson@me.com>
2016-12-14 10:42:10 -0600

    Removed javascript alert from printing

--- modules/print/barcodeWeb.php
+++ modules/print/barcodeWeb.php
@@ -19,12 +19,6 @@ function sendSKU($sku)
 
             //Send the proper header information along with the request
             http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-            http.onreadystatechange = function() {//Call a function when the state changes.
-            if(http.readyState == 4 && http.status == 200) {
-                alert(http.responseText);
-            }
-        }
         http.send(params);
 		window.location.href = "' .$root.'../../new/index.php?success=true&sku='.$sku.'";
         </script>';
