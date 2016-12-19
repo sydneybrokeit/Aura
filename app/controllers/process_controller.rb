cdce6c8684cc87e5ed23f5c33bcbc5a2a51639ca
Keaton Burleson <keaton.burleson@me.com>
2016-12-19 16:22:30 -0600

    idk

--- app/controllers/process_controller.rb
+++ app/controllers/process_controller.rb
@@ -1,9 +1,18 @@
 class ProcessController < ApplicationController
-  def submit
-    sku = Sku.new
-    sku.json = params
-    sku.save
-    require 'aura-print'
-    AuraPrint.barcodeWeb(sku)
+    def submit
+        sku = Sku.new
+        jsonData = params
+        jsonData['sku'] = generate_sku(19)
+        sku.json = jsonData.to_json
+
+        require 'aura-print'
+        @output = AuraPrint.barcodeWeb(jsonData['sku'], 'Tech')
+        @sku = jsonData['sku']
+          sku.save!
+    end
+
+    def generate_sku(size = 6)
+        charset = %w(2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z)
+        (0...size).map { charset.to_a[rand(charset.size)] }.join
   end
 end
--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,4 +1,26 @@
 class ResultsController < ApplicationController
+  helper_method :generateBarcode
+  helper_method :base64_image
+  require 'rmagick'
+  require 'chunky_png'
+  require 'barby/barcode/code_128'
+  require 'barby/outputter/rmagick_outputter'
+  require 'barby'
   def search
+      @skus = []
+      Sku.all.each do |sku|
+          @skus.push(JSON.parse(sku.json))
+      end
+  end
+
+  def base64_image(image_data)
+      "<img src='data:image/png;base64,#{image_data}' />".html_safe
+  end
+
+  def generateBarcode(sku)
+      barcode = Barby::Code128B.new(sku)
+      @img = barcode.to_image
+      @img.format = 'png'
+      Base64.encode64(@img.to_blob).gsub(/\n/, "")
   end
 end
