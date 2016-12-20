908857bc77e67eda5ba0a454e21dd175ac0aa39e
Harold Schreckengost <harold@haroldmschreckengost.com>
2016-12-20 10:37:22 -0600

    Commit changes backend to use uppercase (Fixes issue #24)

--- results/result/index.php
+++ results/result/index.php
@@ -11,7 +11,8 @@
 
     <title>Aura|<?php
     if (isset($_GET['sku'])) {
-        echo $_GET['sku'];
+        $uppersku = strtoupper($_GET['sku'])
+        echo $uppersku;
     }?></title>
 </head>
 
@@ -37,21 +38,16 @@
             <div class="inner-wrapper">
             <?php
             if (isset($_GET['sku'])) {
-                $jsondata = json_decode(file_get_contents('../data/'.$_GET['sku'].'.json'), true);
+                $jsondata = json_decode(file_get_contents('../data/'.$uppersku.'.json'), true);
 
-                echo "<h1 class='result-title'>Result: ".$_GET['sku'].'</h1>';
+                echo "<h1 class='result-title'>Result: ".$uppersku.'</h1>';
                 echo "<table class='results_properties'>";
                 foreach ($jsondata as $key => $value) {
                     $keyID = $key;
-
                     $key = str_replace('_', ' ', $key);
                     if ($keyID == 'Power_Adaptor') {
                         $value = str_replace('on', 'Yes', $value);
                         $value = str_replace('off', 'No', $value);
-                    }
-                    if ($keyID == 'basic') {
-                        $value = str_replace('basic', 'Indeed!', $value);
-                        $key = 'Basic Functionality';
                     }
 
                     echo "<tr><td class='property_name' id='".$keyID."'>".ucwords($key).":</td><td class='property_value' id='".$keyID."'>".$value.'</td></tr>';
