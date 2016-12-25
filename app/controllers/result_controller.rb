e7d87534ec64e213bcc6c69ea0c10196cb9d830b
Harold Schreckengost <harold@haroldmschreckengost.com>
2016-12-25 10:07:03 -0600

    Modified result page to pull from updated Sku model
    Congrats, self, it freakin' works

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -3,11 +3,10 @@ class ResultController < ApplicationController
     @data = getData(params["sku"])
   end
   def getData(mainSku)
-    Sku.where("sku")
-        return JSON.parse(sku.json)
+      if @sku = Sku.find_by_sku(mainSku)
+        return JSON.parse(@sku.json)
       else
         return "Sorry, that SKU could not be found " + mainSku + sku.json
-    end
+      end
   end
-end
 end
