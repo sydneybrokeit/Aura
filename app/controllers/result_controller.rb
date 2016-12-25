5415a2806834b5ebc09f61831e88d00105fcd74b
Harold Schreckengost <harold@haroldmschreckengost.com>
2016-12-25 09:36:15 -0600

    Moving downstairs for the day >_>

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -1,11 +1,9 @@
 class ResultController < ApplicationController
   def load
     @data = getData(params["sku"])
-    render text: params["sku"]  
   end
   def getData(mainSku)
-    Sku.all.each do |sku|
-      if JSON.parse(sku.json)["sku"] == mainSku
+    Sku.where("sku")
         return JSON.parse(sku.json)
       else
         return "Sorry, that SKU could not be found " + mainSku + sku.json
